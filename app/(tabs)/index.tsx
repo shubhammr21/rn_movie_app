23f230f888d0274e99279d2ed4d9edfb3f202bca
Shubham Maurya <shubhammr21@gmail.com>
2025-06-01 11:43:03 +0530

    feat(home): implement home screen with movie list and search functionality
    Summary: The commit introduces the implementation of the home screen, which includes displaying a list of movies fetched from an API, along with a search bar for searching movies.
    
    Key points:
    
    - Fetches movies data from the API using the custom `useFetch` hook and `fetchMovies` service
    - Renders a `FlatList` to display the movie cards (`MovieCard` component)
    - Adds a `SearchBar` component to allow users to search for movies (navigates to the search screen)
    - Displays loading state while fetching movies data and an error message if an error occurs
    - Styles the home screen with background image, logo, and layout for movie cards
    
    This implementation enhances the app's functionality by providing users with a visually appealing home screen that showcases the latest movies and enables movie search, improving the overall user experience.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,57 @@
-import { Text, View } from "react-native"
+import { useRouter } from "expo-router"
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native"
+
+import MovieCard from "@/components/movie-card"
+import SearchBar from "@/components/search-bar"
+import { icons } from "@/constants/icons"
+import { images } from "@/constants/images"
+import { useFetch } from "@/hooks/use-fetch"
+import { fetchMovies } from "@/services/api"
 
 export default function Index() {
+  const router = useRouter()
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch<DiscoverMovie>(() => fetchMovies({ query: "" }))
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-4xl text-accent">Welcome</Text>
+    <View className="flex-1 bg-primary">
+      <Image className="absolute z-0 w-full" source={images.bg} />
+      <Image source={icons.logo} className="mx-auto mb-5 mt-20 h-10 w-12" />
+      {moviesLoading ? (
+        <ActivityIndicator
+          size="large"
+          color="#0000FF"
+          className="mt-10 self-center"
+        />
+      ) : moviesError ? (
+        <Text>Error : {moviesError?.message}</Text>
+      ) : (
+        <FlatList
+          data={movies?.results}
+          renderItem={({ item }) => <MovieCard {...item} />}
+          keyExtractor={item => item.id.toString()}
+          numColumns={3}
+          columnWrapperClassName="justify-start gap-5 pr-[5] mb-[10]"
+          className="mb-2 flex-1 px-5 pb-32"
+          ListHeaderComponent={
+            <View className="mt-5 flex-1">
+              <SearchBar
+                placeholder="Search for a movie"
+                onPress={() => router.push("/(tabs)/search")}
+              />
+              <Text className="mb-3 mt-5 text-lg font-bold text-white">
+                Latest Movies
+              </Text>
+            </View>
+          }
+          showsVerticalScrollIndicator={false}
+          contentContainerClassName="min-h-full pb-[10]"
+        />
+      )}
     </View>
   )
 }
